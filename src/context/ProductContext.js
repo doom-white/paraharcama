7180bb3994d0fe77d676ea9ce047c3542dfd84d4
doom-white <doom.white@yaani.com>
2023-06-18 16:52:25 +0300

    Create Context

---
+++ src/context/ProductContext.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import Products from "../products.json";
+
+const ProductContext = createContext();
+
+export const useProduct = () => useContext(ProductContext);
+
+const ProductProvider = (props) => {
+  const [money, setMoney] = useState(100);
+  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  const data = {
+    money,
+    setMoney,
+    total,
+    setTotal,
+    cart,
+    setCart,
+    Products,
+  };
+
+  return (
+    <ProductContext.Provider value={data}>
+      {props.children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
